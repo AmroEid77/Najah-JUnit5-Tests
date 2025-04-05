db7669cd898d533650a869b575e0d945848f3fc2
Amro <amro.eidd@gmail.com>
2025-04-05 16:58:09 +0300

    Initialize Product Objects in Test Class for Unit Testing
    - Defined product objects (, , , , , ) with initial prices for use in unit tests.
    - Implemented  to set up test data before each test method and  for cleanup.
    - Used  and  to manage environment setup and teardown for the entire test suite.

--- src/main/najah/test/ProductTest.java
+++ src/main/najah/test/ProductTest.java
@@ -2,21 +2,49 @@ package main.najah.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import main.najah.code.Product;
 
+@DisplayName("Prodcut Tests")
 public class ProductTest {
-    Product p;
+	Product laptop;
+    Product smartphone;
+    Product headphones;
+    Product iphone;
+    Product hyperXCloud;
+    Product airpods;
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @BeforeAll
+    public static void beforeClassStep() {
+        System.out.println("Setting up test environment before executing tests");
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @AfterAll
+    public static void afterClassStep() {
+        System.out.println("Cleaning up test environment after test execution");
+    }
+
+    @BeforeEach
+    public void preTestStep() {
+        // Initialize products with new prices
+        laptop = new Product("MacBook Pro", 2000);
+        smartphone = new Product("Samsung Galaxy S23", 900);
+        headphones = new Product("Beyerdynamic DT 700 PRO X", 350);
+        iphone = new Product("iPhone 16 Pro Max", 1600);
+        hyperXCloud = new Product("HyperX Cloud 2", 100);
+        airpods = new Product("AirPods Pro", 250);
+        System.out.println("Setting up Product objects before test execution");
+    }
+
+    @AfterEach
+    public void endTestStep() {
+        System.out.println("Cleaning up after test execution");
+    }
 
 }
